584fd5e03225cebf0dba1cada6082586562eebda
Simon Krause <simon+github@skrause.me>
2022-05-08 20:00:31 +0200

    feat: migrate to multi-module project

---
+++ app/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.6.21"
+}
+
+group "org.skrause"
+version "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
